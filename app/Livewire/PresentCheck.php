2d7a8524f9cf87b1fae0d0a7b70ce29fdd624a80
wahyudedik <wdedyk@gmail.com>
2024-09-23 07:55:53 +0700

    modified:   app/Livewire/PresentCheck.php 	modified:   resources/views/components/layouts/app.blade.php 	modified:   resources/views/livewire/present-check.blade.php

--- app/Livewire/PresentCheck.php
+++ app/Livewire/PresentCheck.php
@@ -2,23 +2,87 @@
 
 namespace App\Livewire;
 
+use App\Models\ManagementSDM\Attendance;
 use App\Models\ManagementSDM\Employee;
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
 use App\Models\ManagementSDM\Schedule;
+use Illuminate\Support\Carbon;
 
 class PresentCheck extends Component
 {
+
+    public $latitude;
+    public $longitude;
+    public $insideRadius = false;
     public function render()
     {
+        $attendance = Attendance::where('employee_id', Employee::where('user_id', Auth::id())->value('id'))
+                            ->whereDate('created_at', date('Y-m-d'))->first();
         $employee = Employee::where('user_id', Auth::user()->id)->first();
-        $schedules = $employee ? Schedule::where('employee_id', $employee->id)->first() : null;
+        $schedule = $employee ? Schedule::where('employee_id', $employee->id)->first() : null;
         return view(
             'livewire.present-check',
             [
-                'schedules' => $schedules,
+                'schedule' => $schedule,
                 'employee' => $employee,
+                'insideRadius' => $this->insideRadius,
+                'attendance' => $attendance
             ]
         );
+    }
+
+    public function store() 
+    {
+        $this->validate([
+            'latitude' => 'required',
+            'longitude' => 'required'
+        ]);
+
+        $schedule = Schedule::where('user_id', Auth::user()->id)->first();
+
+        $today = Carbon::today()->format('Y-m-d');
+        // $approvedLeave = Leave::where('user_id', Auth::user()->id)
+        //                     ->where('status', 'approved')
+        //                     ->whereDate('start_date', '<=', $today)
+        //                     ->whereDate('end_date', '>=', $today)
+        //                     ->exists();
+
+        // if ($approvedLeave) {
+        //     session()->flash('error', 'Anda tidak dapat melakukan presensi karena sedang cuti');
+        //     return;
+        // }
+
+        if ($schedule) {
+            $attedance = Attendance::where('user_id', Auth::user()->id)
+                            ->whereDate('created_at', date('Y-m-d'))->first();
+            if (!$attedance) {
+                $attedance = Attendance::create([
+                    'user_id' => Auth::user()->id,
+                    'schedule_latitude' => $schedule->office->latitude,
+                    'schedule_longitude' => $schedule->office->longitude,
+                    'schedule_start_time' => $schedule->shift->start_time,
+                    'schedule_end_time' => $schedule->shift->end_time,
+                    'start_latitude' => $this->latitude,
+                    'start_longitude' => $this->longitude,
+                    'start_time' => Carbon::now()->toTimeString(),
+                    'end_time' => Carbon::now()->toTimeString(),
+                ]);
+            } else {
+                $attedance->update([
+                    'end_latitude' => $this->latitude,
+                    'end_longitude' => $this->longitude,
+                    'end_time' => Carbon::now()->toTimeString(),
+                ]);
+            }
+
+            return redirect('admin/attendances');
+
+            // return redirect()->route('presensi', [
+            //     'schedule' => $schedule,
+            //     'insideRadius' => false
+            // ]);
+            
+        }
     }
 }
--- resources/views/livewire/present-check.blade.php
+++ resources/views/livewire/present-check.blade.php
@@ -1,81 +1,112 @@
 <div>
-    {{-- @dd($schedules) --}}
-    <div class="container mx-auto">
-        <div class="bg-white p-6 rounded-lg mt-3 shadow-lg">
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
-                <div>
+    <div class="container mx-auto max-w-sm">
+         <div class="bg-white p-6 rounded-lg mt-3 shadow-lg">
+             <div class="grid grid-cols-1 gap-6 mb-6">
+                 <div>
                     <h2 class="text-2xl font-bold mb-2">Informasi Karyawan</h2>
                     <div class="bg-gray-100 p-4 rounded-lg">
                         <p class="mb-2"><strong>Nama Karyawan : </strong>{{ Auth::user()->name }}</p>
-                        <p class="mb-2"><strong>Kantor : </strong>{{ $schedules->company->name }}</p>
-                        <p class="mb-2"><strong>Shift : </strong>{{ $schedules->shift->name }}
-                            ({{ \Carbon\Carbon::parse($schedules->shift->start_time)->format('H:i') }} -
-                            {{ \Carbon\Carbon::parse($schedules->shift->end_time)->format('H:i') }}) WIB</p>
+                        <p class="mb-2"><strong>Kantor : </strong>{{ $schedule->company->name }}</p>
+                        <p class="mb-2"><strong>Shift : </strong>{{ $schedule->shift->name }}
+                            ({{ \Carbon\Carbon::parse($schedule->shift->start_time)->format('H:i') }} -
+                            {{ \Carbon\Carbon::parse($schedule->shift->end_time)->format('H:i') }}) WIB</p>
+                        @if ($schedule->is_wfa)
+                            <p class="text-green-500"><strong>Status : </strong>WFA</p>
+                        @else
+                            <p><strong>Status : </strong>WFO</p>
+                        @endif
+                    </div>
+                    <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mt-4">
+                        <div class="bg-gray-100 p-4 rounded-lg">
+                            <h4 class="text-l font-bold mb-2">Waktu Datang</h4>
+                            <p><strong>{{ $attendance ? $attendance->start_time : '-' }}</p>
+                        </div>
+                        <div class="bg-gray-100 p-4 rounded-lg">
+                            <h4 class="text-l font-bold mb-2">Waktu Pulang</h4>
+                            <p><strong>{{ $attendance ? $attendance->end_time : '-' }}</p>
+                        </div>
                     </div>
                 </div>
+
                 <div>
-                    <h2 class="text-2xl font-bold mb-2">Informasi Lokasi</h2>
-                    <div id="map" class="mb-4 rounded-lg border border-gray-300"></div>
-                    <button type="button" onclick="checkLocation" class="px-4 py-2 bg-blue-500 text-white rounded">Tag
-                        Location</button>
-                    <button type="button" onclick="isWithinRadius" class="px-4 py-2 bg-green-500 text-white rounded">Submit Present</button>
+                    <h2 class="text-2xl font-bold mb-2">Presensi</h2>
+                    <div id="map" class="mb-4 rounded-lg border border-gray-300" wire:ignore></div>
+                    @if (session()->has('error'))
+                        <div style="color: red; padding: 10px; border: 1px solid red; backgroud-color: #fdd;">
+                            {{ session('error') }}
+                        </div>
+                    @endif
+                    <form class="row g-3 mt-3" wire:submit="store" enctype="multipart/form-data">
+                        <button type="button" onclick="tagLocation()"
+                            class="px-4 py-2 bg-blue-500 text-white rounded">Tag Location</button>
+                        @if ($insideRadius)
+                            <button type="submit" class="px-4 py-2 bg-green-500 text-white rounded">Submit
+                                Presensi</button>
+                        @endif
+                    </form>
                 </div>
+
             </div>
         </div>
+
     </div>
-    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
     <script>
-        const map = L.map('map').setView([{{ $schedules->company->latitude }}, {{ $schedules->company->longitude }}], 13);
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+        let map;
+        let lat;
+        let lng;
+        const company = [{{ $schedule->company->latitude }}, {{ $schedule->company->longitude }}];
+        const radius = {{ $schedule->company->radius }};
+        let component;
+        let marker;
+        document.addEventListener('livewire:initialized', function() {
+            component = @this;
+            map = L.map('map').setView([{{ $schedule->company->latitude }}, {{ $schedule->company->longitude }}],
+                15);
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-        const center = L.latLng({{ $schedules->company->latitude }},
-            {{ $schedules->company->longitude }}); //koordinat company
-        const marker = L.marker(center).addTo(map);
-        marker.bindPopup('{{ $schedules->company->name }}').openPopup();
-        map.setView(center, 13);
+            const circle = L.circle(company, {
+                color: 'red',
+                fillColor: '#f03',
+                fillOpacity: 0.5,
+                radius: radius
+            }).addTo(map);
+        })
 
-        const radius = {{ $schedules->company->radius }}; // Radius in meters
 
-        const circle = L.circle(center, {
-            radius: radius,
-            color: 'red',
-            fillColor: 'red',
-            fillOpacity: 0.2
-        }).addTo(map);
-
-        function checkLocation() {
+        function tagLocation() {
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(function(position) {
-                    const userLat = position.coords.latitude;
-                    const userLng = position.coords.longitude;
+                    lat = position.coords.latitude;
+                    lng = position.coords.longitude;
 
                     if (marker) {
                         map.removeLayer(marker);
                     }
 
-                    marker = L.marker([userLat, userLng]).addTo(map);
-                    map.setView([userLat, userLng], 13);
+                    marker = L.marker([lat, lng]).addTo(map);
+                    map.setView([lat, lng], 15);
 
-                    if (isWithinRadius(userLat, userLng, center, radius)) {
-                        alert('Anda berada di dalam radius');
-                        console.log('Anda berada di dalam radius');
-                    } else {
-                        alert('Anda berada di luar radius');
-                        console.log('Anda berada di luar radius');
+                    if (isWithinRadius(lat, lng, company, radius)) {
+                        component.set('insideRadius', true);
+                        component.set('latitude', lat);
+                        component.set('longitude', lng);
                     }
-
-                });
+                })
             } else {
-                alert('Geolocation is not supported by this browser.');
-                console.log('Geolocation is not supported by this browser.');
+                alert('Tidak bisa get location');
             }
         }
 
-        // Check if the user is within the radius
-        function isWithinRadius(userLat, userLng, center, radius) {
-            let distance = map.distance(center, [userLat, userLng]);
-            return distance <= radius;
+        function isWithinRadius(lat, lng, center, radius) {
+            const is_wfa = {{ $schedule->is_wfa }}
+            if (is_wfa) {
+                return true;
+            } else {
+                let distance = map.distance([lat, lng], center);
+                return distance <= radius;
+            }
+
         }
     </script>
 </div>
